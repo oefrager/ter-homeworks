9aa1ddb908a420c1e3861712d572f3fdf57abfad
goi <goi@belkam.com>
2025-02-18 17:08:50 +0400

    ter-homeworks

--- 03/outputs.tf
+++ 03/outputs.tf
@@ -1,38 +1,15 @@
-output "default_instance_public_ip" {
-    value = yandex_compute_instance.count.network_interface[0].nat_ip_address
+output "webservers" {
+  value = [for i in yandex_compute_instance.count[*] : {
+    name  = i.name
+    id    = i.id
+    fqdn  = i.fqdn
+  }]
 }
-%{~ for i in webservers ~}
-${i["name"]}   ansible_host=${i["network_interface"][0]["nat_ip_address"]}
-%{~ endfor ~}
 
-#test_list = ["develop", "staging", "production"]
-
-  test = [
-    {
-      "dev1" = [
-        "ssh -o 'StrictHostKeyChecking=no' ubuntu@62.84.124.117",
-        "10.0.1.7",
-      ]
-    },
-    {
-      "dev2" = [
-        "ssh -o 'StrictHostKeyChecking=no' ubuntu@84.252.140.88",
-        "10.0.2.29",
-      ]
-    },
-    {
-      "prod1" = [
-        "ssh -o 'StrictHostKeyChecking=no' ubuntu@51.250.2.101",
-        "10.0.1.30",
-      ]
-    },
-  ]
-
-
-
-
-#output "subnet_id" {
- #   value = data.yandex_vpc_subnet.default_a.subnet_id
-#}
-
-
+output "databases" {
+  value = [for v in yandex_compute_instance.db : {
+    name = v.name
+    id   = v.id
+    fqdn = v.fqdn
+  }]
+}
