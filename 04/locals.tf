af859a881cfd7bb19644782a4876de801b7af71b
goi <goi@belkam.com>
2025-02-21 16:33:11 +0400

    222

---
+++ 04/locals.tf
@@ -0,0 +1,7 @@
+###ssh vars
+locals {
+    metadata = {
+        serial-port-enable = 1
+        ssh-keys           = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+    }
+}
---
+++ 04/main.tf
@@ -0,0 +1,54 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+module "marketing" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "marketing"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "webserv"
+  instance_count = 1
+  image_family   = var.image_family
+  public_ip      = true
+
+  labels = { 
+    project = "marketing"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+#  metadata = local.metadata
+}
+
+module "analytics" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "analytics"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "stage"
+  instance_count = 1
+  image_family   = var.image_family
+  public_ip      = true
+
+  labels = { 
+    project = "analytics"
+  }
+
+#  metadata = local.metadata
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+}
