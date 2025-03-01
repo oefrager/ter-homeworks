0bc1b3e151403d745ba8c9cb0aede21fa198b9d2
Oleg <83029093+oefrager@users.noreply.github.com>
2025-03-01 22:59:17 +0400

    Update main.tf

--- 04/main.tf
+++ 04/main.tf
@@ -11,7 +11,7 @@ module "vpc_prod" {
   subnets = [
     { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
     { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
-    { zone = "ru-central1-c", cidr = "10.0.3.0/24" },
+    { zone = "ru-central1-d", cidr = "10.0.3.0/24" },
   ]
 }
 
